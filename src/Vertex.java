1f3d7cd6324161a1c86cd6b5884633cd9d341cf2
beka24k <110625229+beka24k@users.noreply.github.com>
2023-05-30 15:56:41 +0600

    vertex

---
+++ src/Vertex.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Represents a vertex in a graph.
+ *
+ * @param <V> the type of data stored in the vertex
+ */
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjVertex;
+
+    /**
+     * Constructs a vertex with the specified data.
+     *
+     * @param data the data associated with the vertex
+     */
+    public Vertex(V data) {
+        this.data = data;
+        this.adjVertex = new HashMap<>();
+    }
+
+}
