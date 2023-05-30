2486679012f22c052ce144840b24afb8801732ef
beka24k <110625229+beka24k@users.noreply.github.com>
2023-05-30 15:49:24 +0600

    findpath bfs

--- src/BFS.java
+++ src/BFS.java
@@ -17,8 +17,75 @@ public class BFS<V> implements Search<V> {
         this.graph = graph;
     }
 
+    /**
+     * Performs breadth-first search starting from the specified start vertex.
+     *
+     * @param startVertex the start vertex for the BFS
+     * @return a list of vertices visited during BFS traversal
+     */
+    public List<Vertex<V>> breadthFirstSearch(Vertex<V> startVertex) {
+        List<Vertex<V>> visited = new ArrayList<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
+
+        visited.add(startVertex);
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> currentVertex = queue.poll();
+            List<WeightedGraph<V>.Edge<V>> edges = graph.getEdge(currentVertex);
+
+            for (WeightedGraph<V>.Edge<V> edge : edges) {
+                Vertex<V> neighborVertex = edge.getDestination();
+                if (!visited.contains(neighborVertex)) {
+                    visited.add(neighborVertex);
+                    queue.add(neighborVertex);
+                }
+            }
+        }
+
+        return visited;
+    }
+
+
+    /**
+     * Finds a path from the source vertex to the destination vertex using BFS.
+     *
+     * @param source      the source vertex
+     * @param destination the destination vertex
+     * @return a list of vertices representing the path from the source to the destination, or an empty list if no path is found
+     */
     @Override
     public List<V> findPath(Vertex<V> source, Vertex<V> destination) {
-        return null;
+        List<Vertex<V>> path = new ArrayList<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
+
+        queue.offer(source);
+        parentMap.put(source, null);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> currentVertex = queue.poll();
+            if (currentVertex.equals(destination)) {
+                // Destination vertex found, reconstruct the path
+                path.add(destination);
+                Vertex<V> parent = parentMap.get(destination);
+                while (parent != null) {
+                    path.add(0, parent);
+                    parent = parentMap.get(parent);
+                }
+                break;
+            }
+
+            List<WeightedGraph<V>.Edge<V>> edges = graph.getEdge(currentVertex);
+            for (WeightedGraph<V>.Edge<V> edge : edges) {
+                Vertex<V> neighborVertex = edge.getDestination();
+                if (!parentMap.containsKey(neighborVertex)) {
+                    queue.offer(neighborVertex);
+                    parentMap.put(neighborVertex, currentVertex);
+                }
+            }
+        }
+
+        return (List<V>) path;
     }
 }
