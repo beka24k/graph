8fb016b8cb091b8c04972ca01525ddbc7e8e35b3
beka24k <110625229+beka24k@users.noreply.github.com>
2023-05-30 16:02:09 +0600

    DijkstraSearch

---
+++ src/DijkstraSearch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/**
+ * DijkstraSearch implements the Search interface using Dijkstra's algorithm to find the shortest path
+ * and distances in a weighted graph.
+ *
+ * @param <V> the type of vertex in the graph
+ */
+public class DijkstraSearch<V>  {
+    private WeightedGraph<V> graph;
+
+    /**
+     * Constructs a DijkstraSearch object with the specified weighted graph.
+     *
+     * @param graph the weighted graph to perform Dijkstra's algorithm on
+     */
+    public DijkstraSearch(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }
+
+
+}
