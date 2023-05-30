30c2a9756c1ca64b26927d99d987bc59977139d1
beka24k <110625229+beka24k@users.noreply.github.com>
2023-05-30 15:52:36 +0600

    WeightedGraph

---
+++ src/WeightedGraph.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A weighted graph implementation that stores vertices and edges with associated weights.
+ *
+ * @param <V> the type of vertex in the graph
+ */
+public class WeightedGraph<V> {
+    private Map<Vertex<V>, List<Edge<V>>> list;
+
+    /**
+     * Constructs an empty weighted graph.
+     */
+    public WeightedGraph() {
+        list = new HashMap<>();
+    }
+
+    /**
+     * Represents an edge in the weighted graph.
+     *
+     * @param <V> the type of vertex in the graph
+     */
+    public class Edge<V> {
+        private Vertex<V> source;
+        private Vertex<V> destination;
+        private double weight;
+
+        /**
+         * Constructs an edge with the specified source, destination, and weight.
+         *
+         * @param source      the source vertex of the edge
+         * @param destination the destination vertex of the edge
+         * @param weight      the weight associated with the edge
+         */
+        public Edge(Vertex<V> source, Vertex<V> destination, double weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+
+        /**
+         * Returns the source vertex of the edge.
+         *
+         * @return the source vertex
+         */
+        public Vertex<V> getSource() {
+            return source;
+        }
+
+
+    }
+
+
+}
